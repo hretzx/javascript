02fd2273c29c7241dd2359921470aafb43dc3fbf
Hritvi Narvekar <hritvi.narvekar@vit.edu.in>
2025-12-12 20:19:43 +0530

    sols

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,5 +1,5 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (typeof number=="number") {
     return true;
   } else {
     return false;
--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(s,numb) {
+    if(numb<0){
+        return "Error";
+    }
+    else{
+        let j=1;
+        let string="";
+        while(j<=numb){
+            string=string+s;
+            j++;
+        }
+        return string;
+    }
 };
 
 // Do not edit below this line
